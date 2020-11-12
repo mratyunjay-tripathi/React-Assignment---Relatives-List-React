4b6879d67dfb9f2d731a5fe2b0a0228553585423
Mratyunjay <tripathi.mratyunjay.1@gmail.com>
2020-11-12 13:10:13 +0530

    commit with working

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,22 @@ import '../styles/App.css';
 
 class App extends Component {
     
-    const relatives = ["chacha", "mama", "phupha", "bua"];
+ relatives = ["chacha", "mama", "phupha", "bua"];
 
    
-
+    render(){
+        console.log("hello");
         return(
             <div id="main">
                <ol key="relativeList">
-                {relatives.map((relative, indx) => (
+                {this.relatives.map((relative, indx) => (
           <li key={"relativeListItem" + (indx + 1)}>{relative}</li>
                  ))}
                  </ol>
             </div>
         );
-    
-}
+     }
+};
 
 
 export default App;
